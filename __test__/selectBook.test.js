ddf16520d109e2526b6ecc4aca61365e9a26b259
Sara McMullin <mcsara1221@gmail.com>
2019-11-18 11:39:26 -0600

    Git is being finicky about my commit message, new test made

--- __test__/selectBook.test.js
+++ __test__/selectBook.test.js
@@ -1,13 +1,20 @@
 
 require('./selectBook')
 
-const mockFunction =jest.fn()
+// const mockFunction =jest.fn()
 
-mockFunction.mockReturnValue();
-const resultmockFunction();
+// mockFunction.mockReturnValue();
+// const resultmockFunction();
 
 // describe("selectBook", () => {
 //     test("it should filter by a choices array", () => {
     
    
 //    }
+
+
+test('bookSelected', () => {
+    let bookSelected = null;
+    expect(bookSelected).not.toBeTruthy();
+    expect(bookSelected).toBeFalsy();
+  });
